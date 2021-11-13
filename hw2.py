8e5f6cc5efe7aa3450d2f0ccf405635a0bc06c99
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-13 17:40:34 +0500

    first commit

---
+++ hw2.py
@@ -0,0 +1,57 @@
+#1
+# a = int(input("enter number: "))
+# if 0 == a : print("true")
+# else: print("false")
+
+#2
+# a = int(input("enter number: "))
+# if 0 < a : print("true")
+# else: print("false")
+
+#3
+# a = int(input("enter number: "))
+# if 0 > a : print("true")
+# else: print("false")
+
+#4
+# a = int(input("enter number: "))
+# if 0 < a : print("true")
+# elif 0 == a : print("true")
+# else: print("false")
+
+#5
+# a = int(input("enter number: "))
+# if 0 > a : print("true")
+# elif 0 == a : print("true")
+# else: print("false")
+
+#6
+# a = int(input("enter number: "))
+# if 0 != a : print("true")
+# else: print("false")
+
+#7
+# a = str(input("enter number: "))
+# if "test" == a : print("true")
+# else: print("false")
+
+#8
+# a = int(str(input("enter number: ")))
+# if '1' == a : print("true")
+# elif 1 == a : print("true")
+# else: print("false")
+
+#9. Пользователь вводит порядковый номер пальца руки.
+#  Необходимо показать его название на экран.
+# a = int(input("Inter Finger Number: "))
+# if 1 == a: print("First Finger")
+# elif 2 == a: print("Second Finger")
+# elif 3 == a: print("Third Finger")
+# elif 4 == a: print("Fourth Finger")
+# elif 5 == a: print("Fifth Finger")
+# elif 6 == a: print("Sixth Finger")
+# elif 7 == a: print("Seventh Finger")
+# elif 8 == a: print("Eighth Finger")
+# elif 9 == a: print("Ninth Finger")
+# elif 10 == a: print("Tenth Finger")
+# else : print("THERE IS NO SUCH A FINGER(")
---
+++ hw3.py
@@ -0,0 +1,14 @@
+
+x = int(input("Vvedite Nomer: "))
+if x % 5 == 0:
+    print('X delitsya na 5')
+if x % 2 == 0:
+    print('X-chetnoe chislo')
+
+
+if x % 2 == 0:
+    print('X-chetnoe chislo')
+elif x % 5 == 0:
+    print('X delitsya na 5')    
+
+
