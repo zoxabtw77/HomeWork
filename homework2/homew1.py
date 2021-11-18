8cb4edea7c44074d1ede60c5ef1234dc18c0b03b
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-18 17:37:30 +0500

    homework for 16.11

---
+++ homework2/homew1.py
@@ -0,0 +1,7 @@
+a=float(input('enter nember'))
+b=float(input('enter nember'))
+c=float(input('enter nember'))
+d=float(input('enter nember'))
+
+if a > b > c > d: print(a, b, c, d)
+print(a, b, c, d)
